13775de08c9c5d363f618d9dfffe25b428af2db6
Michal Olszewski <mi.olszewski@gmail.com>
2014-04-25 23:18:14 +0100

    pull-analyser library available in jasmine-node tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,26 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jasmine_node: {
+            options: {
+                forceExit: true,
+                matchall: false,
+                jUnit: {
+                    report: true,
+                    savePath : "./build/reports/jasmine/",
+                    useDotNotation: true,
+                    consolidate: true
+                }
+            },
+            all: ['test/']
+        }
     });
 
     // These plugins provide necessary tasks.
     require('load-grunt-tasks')(grunt);
 
+    // Test task
+    grunt.registerTask('test', ['jasmine_node']);
     // Default task.
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['test']);
 };
---
+++ src/pull-analyser.js
@@ -0,0 +1,14 @@
+//     pull-analyser.js 0.1.0
+//     (c) 2014 Michal Olszewski
+//     pull-analyser may be freely distributed under the MIT license.
+(function (undefined) {
+
+    var VERSION = '0.1.0';
+    var pull_analyser = function() {
+        // empty for now
+    };
+    pull_analyser.VERSION = VERSION;
+
+    // NodeJS
+    module.exports = pull_analyser;
+}).call(this);
