506180f1c88102172237f165eeada0985a4b658d
Michal Olszewski <mi.olszewski@gmail.com>
2014-05-28 00:13:34 +0100

    Analysing closed pull requests

--- src/pull-analyser.js
+++ src/pull-analyser.js
@@ -5,11 +5,39 @@
 
 (function (undefined) {
     var VERSION = '0.1.0';
+    var _ = require('underscore');
 
     var pullAnalyser = function() {
         // empty for now
     };
     pullAnalyser.VERSION = VERSION;
+
+    var extractDate = function(pullRequestDate) {
+        return pullRequestDate.substring(0, 10);
+    };
+
+    pullAnalyser.analyseClosed = function(pullRequests) {
+
+        var mapping = _.chain(pullRequests)
+            .pluck('closed_at')
+            .filter(function(closedAt) {
+                return _.isString(closedAt);
+            })
+            .map(function(closedAt) {
+                return extractDate(closedAt);
+            })
+            .reduce(function(memo, date) {
+                if (!_.isNumber(memo[date])) {
+                    memo[date] = 0;
+                }
+                memo[date] += 1;
+
+                return memo;
+            }, {})
+            .value();
+
+        return mapping;
+    };
 
     // NodeJS
     module.exports = pullAnalyser;
---
+++ test/closedPullRequestsSpec.js
@@ -0,0 +1,39 @@
+"use strict";
+
+describe("Closed pull requests are analysed", function () {
+    var pullAnalyser = require('../src/pull-analyser');
+
+    it("should return mapping between closing date and number of pull requests", function () {
+        var result = pullAnalyser.analyseClosed([
+            {
+                "closed_at": "2014-04-11T06:33:38Z"
+            }
+        ]);
+
+        expect(result).toEqual({
+            "2014-04-11": 1
+        });
+    });
+
+    it("should return no mapping when pull requests are empty", function () {
+        var result = pullAnalyser.analyseClosed([]);
+
+        expect(result).toEqual({});
+    });
+
+    it("should filter not closed pull requests", function () {
+        var result = pullAnalyser.analyseClosed([{
+            "closed_at": null
+        }]);
+
+        expect(result).toEqual({});
+    });
+
+    it("should filter not valid pull requests", function () {
+        var result = pullAnalyser.analyseClosed([{
+            "closed_on": "2013-04-13"
+        }]);
+
+        expect(result).toEqual({});
+    });
+});
